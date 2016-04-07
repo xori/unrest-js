70034081d29c1490807548391812c87ce594a329
Evan Verworn <evan.verworn@dsbn.org>
2016-04-07 16:10:16 -0400

    linting

--- src/request.js
+++ src/request.js
@@ -1,12 +1,12 @@
 var xhr = require('superagent');
 var odata = require('./odata');
 
-function jsonify(agent) {
+function jsonify (agent) {
   return agent.type('application/json')
     .accept('json');
 }
 
-function handleResponses(request) {
+function handleResponses (request) {
   request._status = 'pending';
   request.pending = true;
   var method = request._agent.method;
@@ -22,7 +22,7 @@ function handleResponses(request) {
     if (cache) {
       // if the cache exists and isn't old
       if (cache.time && request._cache > +(new Date()) - cache.time) {
-        request._onSuccess.forEach(function(cb) {
+        request._onSuccess.forEach(function (cb) {
           cb(cache.data, true);
         });
         request._cachedata = cache.data;
@@ -34,13 +34,13 @@ function handleResponses(request) {
 
   // //
   // Handle Response
-  request._agent.end(function(err, res) {
+  request._agent.end(function (err, res) {
     request._status = 'resolved';
     delete request.pending;
     if (err) { // on error
       request.error = err;
       // debugger;
-      request._onError.forEach(function(cb) {
+      request._onError.forEach(function (cb) {
         cb(err);
       });
     } else { // on success
@@ -53,7 +53,7 @@ function handleResponses(request) {
       }
       // //
       // perform injection
-      if (typeof(res.body) === 'object' && !Array.isArray(res.body)) {
+      if (typeof (res.body) === 'object' && !Array.isArray(res.body)) {
         for (var prop in request._data) {
           request[prop] = request._data[prop];
         }
@@ -62,111 +62,112 @@ function handleResponses(request) {
       }
       // //
       // perform calbacks.
-      request._onSuccess.forEach(function(cb) {
+      request._onSuccess.forEach(function (cb) {
         cb(res.body);
       });
     }
-    if( request._table._database.onEnd ) request._table._database.onEnd();
+    if (request._table._database.onEnd) request._table._database.onEnd();
 
   });
 }
 
-module.exports = class Request {
-  constructor(table) {
-    this._table = table;
-    this._status = 'idle';
-    this._onSuccess = [];
-    this._onError = [];
-    if (this._table._database.cacheByDefault) {
-      this._cache = this._table._database.cacheTTL;
+module.exports =
+  class Request {
+    constructor(table) {
+      this._table = table;
+      this._status = 'idle';
+      this._onSuccess = [];
+      this._onError = [];
+      if (this._table._database.cacheByDefault) {
+        this._cache = this._table._database.cacheTTL;
+      }
     }
-  }
 
-  cacheable(lifetime) {
-    if (this._status !== 'idle' && this._status !== 'pending') {
-      console.error('cacheable could not be set before request was sent out. Ensure this is called before query, fetch, save, then, etc');
+    cacheable(lifetime) {
+      if (this._status !== 'idle' && this._status !== 'pending') {
+        console.error('cacheable could not be set before request was sent out. Ensure this is called before query, fetch, save, then, etc');
+      }
+      if (!lifetime) {
+        lifetime = this._table._database.cacheTTL;
+      }
+      this._cache = lifetime;
+      return this;
     }
-    if (!lifetime) {
-      lifetime = this._table._database.cacheTTL;
+
+    odata() {
+      return this._table._database.odata;
     }
-    this._cache = lifetime;
-    return this;
-  }
 
-  odata() {
-    return this._table._database.odata;
-  }
-
-  resource(id) {
-    id = id.toString();
-    return this._table.url + (this.odata() ? '(' + id + ')' : '/' + id);
-  }
-  // QUERY GET /table/
-  // Returns a list.
-  query(options) {
-    this._agent = xhr
-      .get(this._table.url
-        + ( this.odata() ? "?" + (new odata(this._table._query)).toString() : ""))
-      .query(options);
-    jsonify(this._agent);
-    if (this._status === 'idle') handleResponses(this);
-    return this;
-  }
-
-  fetch(id, options) {
-    this._agent = xhr
-      .get( this.resource(id) )
-      .query(options);
-    jsonify(this._agent);
-    if (this._status === 'idle') handleResponses(this);
-    return this;
-  }
-
-  save(obj) {
-    var r = null;
-    var id = obj.Id || obj.id;
-    if (!id || id === 0) {
-      r = xhr.post(this._table.url);
-    } else {
-      r = xhr.put(this.resource(id));
+    resource(id) {
+      id = id.toString();
+      return this._table.url + (this.odata() ? '(' + id + ')' : '/' + id);
     }
-    this._agent = r.send(obj);
-    jsonify(this._agent);
-    if (this._status === 'idle') handleResponses(this);
-    return this;
-  }
-
-  remove(Id) {
-    this._agent = xhr.del(this.resource(Id));
-    jsonify(this._agent);
-    if (this._status === 'idle') handleResponses(this);
-    return this;
-  }
-
-  then(cb) {
-    if (this._status === 'resolved' && !this.error) {
-      cb(this._data);
-    } else {
-      this._onSuccess.push(cb);
+    // QUERY GET /table/
+    // Returns a list.
+    query(options) {
+      this._agent = xhr
+        .get(this._table.url
+          + (this.odata() ? '?' + (new odata(this._table._query)).toString() : ''))
+        .query(options);
+      jsonify(this._agent);
+      if (this._status === 'idle') handleResponses(this);
+      return this;
     }
-    if (this._status === 'idle') handleResponses(this);
-    if (this._status === 'pending' && this._cachedata) {
-      cb(this._cachedata, true);
-    }
-    return this;
-  }
 
-  catch (cb) {
-    if (this._status === 'resolved' && this.error) {
-      cb(this.error);
-    } else {
-      this._onError.push(cb);
+    fetch(id, options) {
+      this._agent = xhr
+        .get(this.resource(id))
+        .query(options);
+      jsonify(this._agent);
+      if (this._status === 'idle') handleResponses(this);
+      return this;
     }
-    if (this._status === 'idle') handleResponses(this);
-    return this;
-  }
 
-  status() {
-    return this._status;
-  }
+    save(obj) {
+      var r = null;
+      var id = obj.Id || obj.id;
+      if (!id || id === 0) {
+        r = xhr.post(this._table.url);
+      } else {
+        r = xhr.put(this.resource(id));
+      }
+      this._agent = r.send(obj);
+      jsonify(this._agent);
+      if (this._status === 'idle') handleResponses(this);
+      return this;
+    }
+
+    remove(Id) {
+      this._agent = xhr.del(this.resource(Id));
+      jsonify(this._agent);
+      if (this._status === 'idle') handleResponses(this);
+      return this;
+    }
+
+    then(cb) {
+      if (this._status === 'resolved' && !this.error) {
+        cb(this._data);
+      } else {
+        this._onSuccess.push(cb);
+      }
+      if (this._status === 'idle') handleResponses(this);
+      if (this._status === 'pending' && this._cachedata) {
+        cb(this._cachedata, true);
+      }
+      return this;
+    }
+
+    catch(cb) {
+      if (this._status === 'resolved' && this.error) {
+        cb(this.error);
+      } else {
+        this._onError.push(cb);
+      }
+      if (this._status === 'idle') handleResponses(this);
+      return this;
+    }
+
+    status() {
+      return this._status;
+    }
 };
